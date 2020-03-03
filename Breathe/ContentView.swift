812d3fa76936110ce2665b442c15ad11217bf2b7
Nigel Gee <nigel.gee@live.com>
2020-03-03 17:28:48 +0000

    Commit rotate flower

--- Breathe/ContentView.swift
+++ Breathe/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var radius: CGFloat = 140
+    @State private var rotate: Double = 1
     
     var body: some View {
         ZStack {
@@ -23,13 +24,14 @@ struct ContentView: View {
                 Circle()
                     .frame(width: self.radius)
                     .offset(CGSize(width: 0, height: -self.radius / 2 + 15))
-                    .rotationEffect(Angle(radians: .pi / 3 * Double(petal)))
+                    .rotationEffect(Angle(radians: .pi / 3 * Double(petal) + self.rotate))
                     .foregroundColor(.blue)
                     .opacity(0.4)
             }
             .onAppear {
                 withAnimation(Animation.easeInOut(duration: 3).repeatForever()) {
                     self.radius = 15
+                    self.rotate += 1
                 }
             }
         }
