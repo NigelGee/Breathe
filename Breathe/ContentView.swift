f91501a7835eed6b359bdae80f1468dffb40d34b
Nigel Gee <nigel.gee@live.com>
2020-03-04 16:49:13 +0000

    Commit Breathe with Swift over Coffee

--- Breathe/ContentView.swift
+++ Breathe/ContentView.swift
@@ -15,24 +15,34 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.black
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .frame(width: 25)
-                .foregroundColor(.blue)
-            
-            ForEach(0..<6) { petal in
-                Circle()
-                    .frame(width: self.radius)
-                    .offset(CGSize(width: 0, height: -self.radius / 2 + 15))
-                    .rotationEffect(Angle(radians: .pi / 3 * Double(petal) + self.rotate))
-                    .foregroundColor(.blue)
-                    .opacity(0.4)
-            }
-            .onAppear {
-                withAnimation(Animation.easeInOut(duration: 3).repeatForever()) {
-                    self.radius = 15
-                    self.rotate -= 1
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Breathe with")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                ZStack {
+                    Circle()
+                        .frame(width: 25)
+                        .foregroundColor(.blue)
+                    
+                    ForEach(0..<6) { petal in
+                        Circle()
+                            .frame(width: self.radius)
+                            .offset(CGSize(width: 0, height: -self.radius / 2 + 15))
+                            .rotationEffect(Angle(radians: .pi / 3 * Double(petal) + self.rotate))
+                            .foregroundColor(.blue)
+                            .opacity(0.4)
+                    }
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 3).repeatForever()) {
+                            self.radius = 15
+                            self.rotate -= 1
+                        }
+                    }
                 }
+                Text("Swift over Coffee")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
             }
         }
     }
